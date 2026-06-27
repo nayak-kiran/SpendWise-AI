ac312d26cff8334b81133d9d55a1b356d0f6100d
Kiran Nayak <kirannayak0406@gmail.com>
2026-06-27 19:45:52 +0530

    feat: add validation schemas

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,1 +1,51 @@
- 
+from pydantic import BaseModel, EmailStr
+from datetime import date as date_type
+
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str 
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+    class Config:
+        from_attributes = True
+
+class ExpenseCreate(BaseModel):
+    amount: float
+    description: str
+    date: date_type
+
+class ExpenseOut(BaseModel):
+    id: int
+    amount: float
+    description: str
+    category: str
+    date: date_type
+
+    class Config:
+        from_attributes = True
+
+class BudgetCreate(BaseModel):
+    month: str
+    amount: float
+
+
+class BudgetOut(BaseModel):
+    id: int
+    month: str
+    amount: float
+
+    class Config:
+        from_attributes = True
+        
+
