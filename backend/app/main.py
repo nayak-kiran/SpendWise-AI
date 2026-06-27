c49206de5cdd7b88e56b42b9d076fe4abe09597b
Kiran Nayak <kirannayak0406@gmail.com>
2026-06-27 15:27:25 +0530

    feat: create database models and tables

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from app.database import engine, Base
+from app import models
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"message": "SpendWise AI backend is running"} 
+    return {"message": "SpendWise AI backend is running"}
+
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,44 @@
- 
+from sqlalchemy import Column, Integer, String, DateTime, Float, Date, ForeignKey
+from sqlalchemy.sql import func
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+
+class Expense(Base):
+    __tablename__ = "expenses"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    amount = Column(Float, nullable=False)
+    description = Column(String, nullable=False)
+    category = Column(String, default="Uncategorized")
+    date = Column(Date, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+class Budget(Base):
+    __tablename__ = "budgets"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    month = Column(String, nullable=False)
+    amount = Column(Float, nullable=False)
+
+class AlertSent(Base):
+    __tablename__ = "alerts_sent"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    month = Column(String, nullable=False)
+    threshold = Column(Integer, nullable=False)
+    sent_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    
