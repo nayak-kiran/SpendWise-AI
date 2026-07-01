2550272698ebcb501d4884034971b8b5440afc6f
Kiran Nayak <kirannayak0406@gmail.com>
2026-07-01 08:28:28 +0530

    feat: configure CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,10 +2,18 @@ from fastapi import FastAPI
 from app.database import engine, Base
 from app import models
 from app.routes import auth , expenses , budgets
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(expenses.router, prefix="/expenses", tags=["Expenses"])
 app.include_router(budgets.router, prefix="/budgets", tags=["Budgets"])
